f8898430f7268183c40dea4c08e8132e3dde7fd3
Erick Madrigal Flores <erick.madfl@gmail.com>
2024-07-25 08:55:20 -0600

    se cambia color

--- src/views/AreaUnoView.vue
+++ src/views/AreaUnoView.vue
@@ -41,69 +41,69 @@
           style="fill:#ffffff;fill-opacity:0.5;stroke:#000000;stroke-width:2.065"
           d="M 1.4612546,5.8450183 2.9225091,279.09962 781.77122,284.94466 1904.928,41.280442 l 2.0093,-4.931733 2.0092,-6.94096 -1.8266,-7.48893 -2.9225,-5.297048 -3.6531,-4.01845 -6.2104,-5.2970481 z"
           id="path1" /><path
-          style="fill:#00ff00;fill-opacity:0.5;stroke:#000000;stroke-width:2.05203"
+          style="fill:#0CD464;fill-opacity:0.5;stroke:#000000;stroke-width:2.05203"
           d="M 137.25208,15.415547 H 250.33203 L 249.06957,142.19241 137.07172,141.64439 Z"
-          id="path2" data-color="#00ff00"  @click="handleClick"/><path
-          style="fill:#ff0000;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
+          id="path2" data-color="#0CD464"  @click="handleClick"/><path
+          style="fill:#F2135D;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
           d="m 250.12934,15.1853 h 112.35704 l -1.25439,126.7834 -111.28185,-0.54805 z"
-          id="path3" data-color="#ff0000"  @click="handleClick"/><path
-          style="fill:#00ff00;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
+          id="path3" data-color="#F2135D"  @click="handleClick"/><path
+          style="fill:#0CD464;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
           d="M 362.212,15.639291 H 474.56904 L 473.31465,142.42269 362.0328,141.87464 Z"
-          id="path4" data-color="#00ff00"  @click="handleClick"/><path
-          style="fill:#00ff00;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
+          id="path4" data-color="#0CD464"  @click="handleClick"/><path
+          style="fill:#0CD464;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
           d="M 474.33088,15.048332 H 586.68792 L 585.43353,141.83173 474.15168,141.28368 Z"
-          id="path5" data-color="#00ff00"  @click="handleClick"/><path
-          style="fill:#00ff00;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
+          id="path5" data-color="#0CD464"  @click="handleClick"/><path
+          style="fill:#0CD464;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
           d="M 586.91301,16.258426 H 699.27005 L 698.01566,143.04183 586.73381,142.49378 Z"
-          id="path6" data-color="#00ff00"  @click="handleClick"/><path
-          style="fill:#00ff00;fill-opacity:0.5;stroke:#000000;stroke-width:2.05528"
+          id="path6" data-color="#0CD464"  @click="handleClick"/><path
+          style="fill:#0CD464;fill-opacity:0.5;stroke:#000000;stroke-width:2.05528"
           d="m 700.1452,17.353935 h 113.44141 l -1.2665,126.773445 -112.35584,-0.54801 z"
-          id="path7" data-color="#00ff00"  @click="handleClick"/><path
-          style="fill:#00ff00;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
+          id="path7" data-color="#0CD464"  @click="handleClick"/><path
+          style="fill:#0CD464;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
           d="M 813.22989,17.089784 H 925.58693 L 924.33254,143.87304 813.05069,143.32499 Z"
-          id="path8" data-color="#00ff00"  @click="handleClick"/><path
-          style="fill:#ff0000;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
+          id="path8" data-color="#0CD464"  @click="handleClick"/><path
+          style="fill:#F2135D;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
           d="m 925.56906,17.143534 h 112.35704 l -1.2544,126.783256 -111.28184,-0.54805 z"
-          id="path9" data-color="#ff0000"  @click="handleClick"/><path
-          style="fill:#00ff00;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
+          id="path9" data-color="#F2135D"  @click="handleClick"/><path
+          style="fill:#0CD464;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
           d="m 1038.3871,17.394239 h 112.3571 l -1.2544,126.783261 -111.2819,-0.54805 z"
-          id="path10" data-color="#00ff00"  @click="handleClick"/><path
-          style="fill:#ff0000;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
+          id="path10" data-color="#0CD464"  @click="handleClick"/><path
+          style="fill:#F2135D;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
           d="m 1151.2571,17.741646 h 112.3571 l -1.2544,126.783254 -111.2819,-0.54805 z"
-          id="path11" data-color="#ff0000"  @click="handleClick"/><path
-          style="fill:#00ff00;fill-opacity:0.5;stroke:#000000;stroke-width:2.065"
+          id="path11" data-color="#F2135D"  @click="handleClick"/><path
+          style="fill:#0CD464;fill-opacity:0.5;stroke:#000000;stroke-width:2.065"
           d="M 22.649446,15.891144 H 137.17528 l -1.2786,126.763836 -113.429891,-0.54797 z"
-          id="path12" data-color="#00ff00"  @click="handleClick"/><path
-          style="fill:#ff0000;fill-opacity:0.5;stroke:#000000;stroke-width:2.065"
+          id="path12" data-color="#0CD464"  @click="handleClick"/><path
+          style="fill:#F2135D;fill-opacity:0.5;stroke:#000000;stroke-width:2.065"
           d="m 1263.4224,17.823787 -0.5167,126.574723 96.4264,0.2657 16.9871,-3.8358 V 18.083026 Z"
-          id="path13" data-color="#ff0000"  @click="handleClick"/><path
-          style="fill:#00ff00;fill-opacity:0.5;stroke:#000000;stroke-width:2.065"
+          id="path13" data-color="#F2135D"  @click="handleClick"/><path
+          style="fill:#0CD464;fill-opacity:0.5;stroke:#000000;stroke-width:2.065"
           d="m 1376.6845,18.448339 54.797,0.182657 -0.9132,110.690034 -53.7012,12.05535 z"
-          id="path14" data-color="#00ff00"  @click="handleClick"/><path
-          style="fill:#ff0000;fill-opacity:0.5;stroke:#000000;stroke-width:2.065"
+          id="path14" data-color="#0CD464"  @click="handleClick"/><path
+          style="fill:#F2135D;fill-opacity:0.5;stroke:#000000;stroke-width:2.065"
           d="m 22.731787,142.5903 v 125.02484 l 113.142303,1.80819 0.51663,-126.83303 z"
-          id="path15" data-color="#ff0000"  @click="handleClick"/><path
-          style="fill:#00ff00;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
+          id="path15" data-color="#F2135D"  @click="handleClick"/><path
+          style="fill:#0CD464;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
           d="m 137.14973,142.04435 h 112.3571 l -1.2544,126.78325 -111.2819,-0.54805 z"
-          id="path16" data-color="#00ff00"  @click="handleClick"/><path
-          style="fill:#00ff00;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
+          id="path16" data-color="#0CD464"  @click="handleClick"/><path
+          style="fill:#0CD464;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
           d="m 248.92849,142.34918 h 112.3571 l -1.2544,126.78325 -111.2819,-0.54805 z"
-          id="path17" data-color="#00ff00"  @click="handleClick"/><path
-          style="fill:#00ff00;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
+          id="path17" data-color="#0CD464"  @click="handleClick"/><path
+          style="fill:#0CD464;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
           d="m 361.76712,142.03357 h 112.3571 l -1.2544,126.78325 -111.2819,-0.54805 z"
-          id="path18" data-color="#00ff00"  @click="handleClick"/><path
-          style="fill:#00ff00;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
+          id="path18" data-color="#0CD464"  @click="handleClick"/><path
+          style="fill:#0CD464;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
           d="m 474.95799,143.3444 h 112.3571 l -1.2544,126.78325 -111.2819,-0.54805 z"
-          id="path19" data-color="#00ff00"  @click="handleClick"/><path
-          style="fill:#ff0000;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
+          id="path19" data-color="#0CD464"  @click="handleClick"/><path
+          style="fill:#F2135D;fill-opacity:0.5;stroke:#000000;stroke-width:2.04551"
           d="m 587.25416,144.09921 h 112.3571 l -1.2544,126.78325 -111.2819,-0.54805 z"
-          id="path20" data-color="#ff0000"  @click="handleClick"/><path
-          style="fill:#00ff00;fill-opacity:0.5;stroke:#000000;stroke-width:2.065"
+          id="path20" data-color="#F2135D"  @click="handleClick"/><path
+          style="fill:#0CD464;fill-opacity:0.5;stroke:#000000;stroke-width:2.065"
           d="m 699.26075,271.36071 74.5241,-0.25831 38.23073,-8.13696 1.03328,-118.56693 -112.36735,0.51663 z"
-          id="path21" data-color="#00ff00"  @click="handleClick"/><path
-          style="fill:#00ff00;fill-opacity:0.5;stroke:#000000;stroke-width:2.065"
+          id="path21" data-color="#0CD464"  @click="handleClick"/><path
+          style="fill:#0CD464;fill-opacity:0.5;stroke:#000000;stroke-width:2.065"
           d="m 812.66137,263.0946 57.47526,-13.17411 0.25831,-105.00535 -56.44198,-0.77495 z"
-          id="path22" data-color="#00ff00"  @click="handleClick" /></g>
+          id="path22" data-color="#0CD464"  @click="handleClick" /></g>
       </svg>
 
     </b-container>
@@ -124,7 +124,7 @@ export default {
       const color = event.target.getAttribute('data-color');
 
       let statusMessage;
-      if (color === '#00ff00') {
+      if (color === '#0CD464') {
         statusMessage = 'Disponible'; // Verde
       } else {
         statusMessage = 'Ocupado'; // Rojo
